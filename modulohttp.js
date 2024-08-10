90ed88b7f4b4176e09b926e56714e0ae484faa4e
Pedro Guedes <pedroaugustorgg@gmail.com>
2024-08-09 23:51:06 -0300

    Att. Rep. V1.0.1
    Inserção do arquivo formulario.html + Ajustes em códigos dos arquivos modulohttp.js, pagina.html e server.js (com detalhamento via comentários)

--- modulohttp.js
+++ modulohttp.js
@@ -10,7 +10,7 @@ http.createServer(function(req, res){
 //Linha 3: Criando um servidor. Módulo http chamando a função 'createServer' com as funções de 'req' e 'res', onde 'req' (servidor recebendo dados), 'res' (servidor enviando/retornando dados)
 //Linha 4: O res.write serve para escrever algo em tela para o usuário durante a execução do servidor
 //Linha 5: O res.end é necessário pois o servidor não compreende que deve ser encerrado após o res.write. Isso irá forçar a finalização do envio de dados para o servidor
-//Linha 6: Finalização da função 'createServer' + definição de porta para execução do servidor
+//Linha 6: Congiguração de Servidor: - Finalização da função 'createServer' + definição de porta para execução do servidor
 
 //Dica 1: Para executar o servidor, basta executar este arquivo no terminal com o comando: node modulohttp.js
 
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 var express = require('express');
 
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.use(express.static(__dirname + '/complementos'));
 
-app.get('/', function(req, res){ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+/*app.get('/', function(req, res){ 
     res.sendFile(__dirname + '/pagina.html');
+});*/
+
+app.get('/', function(req, res){ 
+    res.sendFile(__dirname + '/formulario.html');
+});
+
+app.post('/receber', function(req, res){
+    res.write('O nome informado eh: '+req.body.nome);
+    res.end();
 });
 
 app.listen(8080);
 
 // Linha 1: Importação do módulo/framework express
-// Linha 3: Importação de função App, presente no módulo express - Para trabalhar com aplicativos do próprio express
-// Linha 5: Especificação do local com CSS e imagens contidas no servidor, o app.use serve para implementar uma ação. O "express.static" serve para especificar um diretório estático
-// Linha 7: Definição da rota. Ao acessar esta roda, a function será executada requisitando dados (através do req) e retornando informações (através do res)
-// Linha 8: A função res está chamando a classe "sendFile" para enviar o arquivo "pagina.html" para o servidor em execução
-// Linha 11: Definição de porta para execução do servidor
+// Linha 3: Importação do módulo/framework body-parser
+// Linha 5: Importação de função App, presente no módulo express - Para trabalhar com aplicativos do próprio express
+// Linha 7: Especificação do local com CSS e imagens contidas no servidor, o app.use serve para implementar uma ação. O "express.static" serve para especificar um diretório estático
+// Linha 9: Configuração do BodyParser - formato de requisição definido como JSON
+// Linha 10: Configuração do BodyParser - Permissão para tratar dados através do método POST
+// Linha 12: Rota - O método GET está sendo utilizado para obtenção de dados do servidor
+// Linha 13: Rota - A função res está chamando a classe "sendFile" para adicionar o arquivo "pagina.html" no servidor em execução
+// Linha 16: Rota - O método GET está sendo utilizado para obtenção de dados do servidor
+// Linha 17: Rota - A função res está chamando a classe "sendFile" para adicionar o arquivo "formulário.html" no servidor em execução
+// Linha 20: Rota - O método POST está sendo utilizado para registrar no servidor
+// Linha 25: Congiguração de Servidor: - Definição de porta para execução do servidor
